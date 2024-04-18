2fa8b03cec0950f093a068625ec3338789eaacb5
Diogo Duarte <diogo.fl.duarte@gmail.com>
2024-04-18 20:46:50 +0100

    last commit before rolling back to a version where data loading starts before the web app

--- simple_raster.py
+++ simple_raster.py
@@ -104,7 +104,7 @@ def parse_contents(contents, filename):
     try:
         if 'csv' in filename:
             return pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-        elif 'npy' in filename:
+        elif filename.endswith('.npy'):
             return np.load(io.BytesIO(decoded), allow_pickle=True)
     except Exception as e:
         print(e)
