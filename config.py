caf35c99fd7a55a3e91f38b7412c6fa6fe4f8afb
Diogo Duarte <diogo.fl.duarte@gmail.com>
2024-04-19 21:51:39 +0100

    just loading files, no buttons for sound yet

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 '''
 I wasted a week of my life making an app that could have been a function call (:
 '''
-ST = r'E:\VIV_23058\S10\VIV_23058_S10_g1\VIV_23058_S10_g1_imec0\kilosort4\spike_times.npy'
-SC = r'E:\VIV_23058\S10\VIV_23058_S10_g1\VIV_23058_S10_g1_imec0\kilosort4\spike_clusters.npy'
-BH = r'E:\processing_S10\VIV_23058_S10_behavioral_descriptor.csv'
-cols = ['sessionwise_time', 'FR_SwOn', 'FR_StOn', 'HR_SwOn', 'HR_StOn', 'FL_SwOn', 'FL_StOn', 'HL_SwOn', 'HL_StOn']
+AP      = r'E:\VIV_23058\S10\VIV_23058_S10_g1\VIV_23058_S10_g1_imec0\VIV_23058_S10_g1_t0.imec0.ap.bin'
+NCHAN   = 385
+ST      = r'E:\VIV_23058\S10\VIV_23058_S10_g1\VIV_23058_S10_g1_imec0\kilosort4\spike_times.npy'
+SC      = r'E:\VIV_23058\S10\VIV_23058_S10_g1\VIV_23058_S10_g1_imec0\kilosort4\spike_clusters.npy'
+BH      = r'E:\processing_S10\VIV_23058_S10_behavioral_descriptor.csv'
+cols    = ['sessionwise_time', 'FR_SwOn', 'FR_StOn', 'HR_SwOn', 'HR_StOn', 'FL_SwOn', 'FL_StOn', 'HL_SwOn', 'HL_StOn']
 # In your main app
 from config import ST, SC, BH
--- simple_raster.py
+++ simple_raster.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 from base64 import b64encode
+import simpleaudio as sa
 from config import *
 
 # Initialize the Dash app
@@ -15,7 +16,7 @@ cols = ['sessionwise_time', 'FR_SwOn', 'FR_StOn', 'HR_SwOn', 'HR_StOn', 'FL_SwOn
 st_file = ST
 sc_file = SC
 bh_file = BH
-
+apbin   = AP
 
 sampling_rate = 30000.0  # Example: 30 kHz
 # Load your data
@@ -23,6 +24,13 @@ print('Reading neural data')
 spike_times     = np.load(st_file)
 spike_times = spike_times.squeeze()/sampling_rate
 spike_clusters  = np.load(sc_file)
+
+print('Mapping ap.bin file')
+if np.size(np.memmap(apbin, np.uint16, 'r')) % NCHAN is not 0:
+    raise ValueError('raw data number of elements not divisible by the default number of channels')
+nsamp = int(np.size(np.memmap(apbin, np.int16, 'r'))/385)
+dat = np.memmap(apbin, np.int16, 'r', shape=(nsamp, NCHAN))
+
 print('Reading behavioral data')
 behavior = pd.read_csv(bh_file, usecols=cols)
 feat = 'FR_SwOn'
@@ -89,6 +97,29 @@ def update_raster(selected_cluster, tmin, tmax, ntrials, msize, feature):
         title='Cluster ' + str(selected_cluster),
     )
     return fig
+
+def normalizeTo1(a):
+    return a/np.max(np.abs(a))
+def play_audio(traces, start_time, duration, channel):
+    SOUNDRATE = 44100
+    sample_rate = int(30000)
+
+    start_index = int(start_time * sample_rate)
+    end_index = start_index + int(duration * sample_rate)
+    channel = int(channel)
+
+    audio_data = traces[start_index:end_index, channel]
+    audio_data = (normalizeTo1(audio_data) * np.iinfo(np.int16).max).astype(np.int16)
+    # audio_data = (audio_data * 32767).astype(np.int16)
+
+    # re-sample to 44100
+    audio_data = np.interp(np.linspace(0, 1, int(duration*SOUNDRATE),   endpoint=False),
+                           np.linspace(0, 1, int(duration*sample_rate), endpoint=False),
+                           audio_data)
+    audio_data = audio_data.astype(np.int16)
+
+    play_obj = sa.play_buffer(audio_data, 1, 2, SOUNDRATE)
+    play_obj.wait_done()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
